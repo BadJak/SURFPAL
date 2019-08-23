098372bf96d19fa15b5676be51ea90dfc11e134c
Benjamin Watrinet <watrinet@gmail.com>
2019-08-23 19:00:01 +0100

    nice profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,27 @@
 class UsersController < ApplicationController
+before_action :find_user, only: [:show, :edit, :update, :destroy]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update(user_params)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :age, :level, :photo, :group_or_solo)
+    params.require(:user).permit(:username, :age, :level)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   devise_for :users
   root to: 'pages#home'
+  resources :users, only: [:show, :edit, :update]
   resources :rides, only: [:index, :show] do
     resources :userrides,  only: [:create]
     resources :messages,  only: [:create]
